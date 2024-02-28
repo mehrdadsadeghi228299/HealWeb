45fe1f92c5887f75cd7a9b7c2d7b6cf7a78b4614
mehrdad <63063456+mehrdadsadeghi228@users.noreply.github.com>
2024-02-28 13:22:13 -0800

    Update city.model.js

--- src/modules/city/city.model.js
+++ src/modules/city/city.model.js
@@ -1,22 +1,22 @@
-
 const { model, Types ,Schema} = require('mongoose');
-
+const SaharTeam=new Schema({
+    name:{type:String, required:true},
+    day:{type:Date, required:true}
+});
 
 const CitySchema = new Schema({
-
     title:{type:String , require:true},
     name:{type:String , require:true},
     description:{type:String , require:true},
     lat:{type:String , require:true},
     lng:{type:String , require:true},
     key:{type:Number , required:true},
-
+    paramters:{type:Object , required:true},
+    SaharTeamCity:{type:[SaharTeam],required:false}
 });
 
 const CityModel = model("City", CitySchema);
 
 module.exports = {
-
     CityModel
-
 }
